b1858604a5c91ff065d89c02229e736fbeb7278e
Firaze <fvattiato9@gmail.com>
2021-08-04 23:09:51 +0200

    test

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ pathway_edges=read_pathway(option)
 if (len(pathway_edges)==0):
     option=last_selection
     skip_calcs=True
+    st.error("Edges not found in eset file, try another pathway!")
 else:
     skip_calcs=False
     last_selection=option
