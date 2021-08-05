8238acb27e82e28df361dd9a3283bb782d89baa4
Firaze <fvattiato9@gmail.com>
2021-08-05 14:37:16 +0200

    fix text

--- app.py
+++ app.py
@@ -70,12 +70,12 @@ else:
                 to_remove.append(x[0])
             else:
                 m=(zeros+minus)/2
-                if ((minus+zeros)/(zeros*minus)>=((minus+zeros)/(m+1)*(m-1))):
+                if ((minus+zeros)/(zeros*minus+1)>=((minus+zeros)/((m+1)*(m-1)+1))):
                     to_remove.append(x[0])
         else:
             essential_edges.append(x[0])
         if (ones==0):
-            signify_values[x[0]]=round((minus+zeros)/(zeros*minus),2)
+            signify_values[x[0]]=round((minus+zeros)/(zeros*minus+1),2)
         else:
             signify_values[x[0]]=0
 
